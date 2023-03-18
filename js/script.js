16223796720251a9c1cf24d281ce04f4d5649953
mahedi6666655555 <mahedinir34678@gmail.com>
2023-03-18 13:19:52 +0600

    wesite is ok

---
+++ js/script.js
@@ -0,0 +1,16 @@
+document.getElementById("btn1").addEventListener("click",function(){
+
+
+    console.log("btn 1 is ok");
+})
+
+document.getElementById("btn2").addEventListener("click",function(){
+
+
+
+
+    prompt("cll ok  ")
+
+
+
+})
